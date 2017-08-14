073d542ccd183bed7b1fb68876cfec792ce77441
Michael Nye <thenyeguy@gmail.com>
2017-08-13 20:47:56 -0700

    Print duration prettier, clean up main a bit.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,20 @@ extern crate nalgebra;
 extern crate palette;
 extern crate rays;
 
+fn print_time(duration: std::time::Duration) {
+    let secs = duration.as_secs();
+    let hours = secs / 3600;
+    let mins = (secs % 3600) / 60;
+    let secs = secs % 60;
+    println!("Rendering took: {}:{:02}:{:02}", hours, mins, secs);
+}
+
 fn main() {
     use rays::prelude::*;
     use std::error::Error;
     use std::time::Instant;
 
-    let matches = clap_app!(rays =>
+    let args = clap_app!(rays =>
         (version: "0.1")
         (author: "Michael Nye <thenyeguy@gmail.com>")
         (about: "Ray tracer in Rust")
@@ -21,18 +29,17 @@ fn main() {
         (@arg reflections: --reflections +takes_value
             "maximum number of reflections per sample")
         (@arg scene: +required "the scene to render")
-    )
-        .get_matches();
+    ).get_matches();
 
     let renderer = Renderer {
-        width: value_t!(matches, "width", u32).unwrap_or(100),
-        height: value_t!(matches, "height", u32).unwrap_or(100),
-        fov: value_t!(matches, "fov", u32).unwrap_or(45),
-        samples_per_pixel: value_t!(matches, "samples", u32).unwrap_or(50),
-        max_reflections: value_t!(matches, "reflections", u32).unwrap_or(5),
+        width: value_t!(args, "width", u32).unwrap_or(100),
+        height: value_t!(args, "height", u32).unwrap_or(100),
+        fov: value_t!(args, "fov", u32).unwrap_or(45),
+        samples_per_pixel: value_t!(args, "samples", u32).unwrap_or(50),
+        max_reflections: value_t!(args, "reflections", u32).unwrap_or(5),
     };
 
-    let scene_name = matches.value_of("scene").unwrap();
+    let scene_name = args.value_of("scene").unwrap();
     let scene = match scene_name {
         "sphere_room" => scenes::sphere_room(),
         _ => {
@@ -43,14 +50,14 @@ fn main() {
 
     let start = Instant::now();
     let img = renderer.render(&scene);
-    println!("Rendering took {} seconds.", start.elapsed().as_secs());
+    print_time(start.elapsed());
 
     let output = format!("images/{}_{}x{}.png",
                          scene_name,
                          renderer.width,
                          renderer.height);
     match img.save(&output) {
-        Ok(()) => println!("Wrote final image to {}", output),
+        Ok(()) => println!("Wrote final image to: {}", output),
         Err(e) => {
             println!("Could not write file: {}", e.description());
             std::process::exit(1);
